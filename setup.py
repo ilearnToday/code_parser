3542c74fa1dff8f60c10c8d8988350987dd7a760
ilearnToday <goodeitime@gmail.com>
2019-05-14 00:37:00 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.MD", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "code_parser",
+    version="1.0",
+    author="Viatcheslav ilearnToday",
+    author_email="goodeitime@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ilearnToday/FirstOtusHomework",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+
+        "License :: MIT License",
+
+        "Operating System :: OS Independent",
+    ],
+)
